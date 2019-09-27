e2cb9aab84bc8b99b444ec6b2dfe63abc9364684
AndJ <xboooox123@gmail.com>
2019-09-28 01:24:59 +0300

    Lesson 1

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -1,6 +1,10 @@
 package lesson1;
 
 import kotlin.NotImplementedError;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaTasks {
@@ -35,7 +39,111 @@ public class JavaTasks {
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortTimes(String inputName, String outputName) {
-        throw new NotImplementedError();
+        class Time implements Comparable {
+            private byte hours, minutes, seconds;
+            private boolean noon; // false = AM, true = PM
+
+            private Time(String str) {
+                String[] parts = str.split("[ :]");
+
+                if (parts.length != 4) throw new IllegalArgumentException(str);
+
+                try {
+                    hours = Byte.parseByte(parts[0]);
+                    minutes = Byte.parseByte(parts[1]);
+                    seconds = Byte.parseByte(parts[2]);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException(str);
+                }
+
+                if ( !(hours >= 0 && hours <= 12) ||
+                     !(minutes >= 0 && minutes <= 59) ||
+                     !(seconds >= 0 && seconds <= 59) ) throw new IllegalArgumentException(str);
+
+                if (parts[3].equals("AM"))
+                    noon = false;
+                else
+                    if (parts[3].equals("PM"))
+                        noon = true;
+                    else
+                        throw new IllegalArgumentException(str);
+            }
+
+            // -1 => this < obj; 0 => this == obj; 1 => this > obj
+            @Override
+            public int compareTo(@NotNull Object obj) {
+                if (this == obj) return 0;
+                if (getClass() != obj.getClass()) throw new IllegalArgumentException();
+
+                Time other = (Time) obj;
+                if (!noon && other.noon) return 1;
+                if (noon && !other.noon) return -1;
+
+                Integer secThis = 3600 * (hours % 12) + 60 * minutes + seconds;
+                Integer secOther = 3600 * (other.hours % 12) + 60 * other.minutes + other.seconds;
+                switch (secThis.compareTo(secOther)) {
+                    case -1:
+                        return 1;
+                    case 1:
+                        return -1;
+                    default:
+                        return 0;
+                }
+            }
+
+            @Override
+            public String toString() {
+                StringBuilder result = new StringBuilder();
+
+                if (hours < 10) result.append("0");
+                result.append(hours).append(":");
+                if (minutes < 10) result.append("0");
+                result.append(minutes).append(":");
+                if (seconds < 10) result.append("0");
+                result.append(seconds).append(" ");
+                if (noon)
+                    result.append("PM");
+                else
+                    result.append("AM");
+
+                return result.toString();
+            }
+        }
+
+        List<Time> input = new ArrayList<>();
+
+        // reading from input file
+        try {
+            Scanner scanner = new Scanner(new File(inputName));
+            while (scanner.hasNext())
+                input.add(new Time(scanner.nextLine()));
+            scanner.close();
+        } catch (IOException e) {
+            System.out.println("File open failed");
+        }
+
+        // insertion sort: O(n) * O(n) = O(n^2)
+        for (int i = 1; i < input.size(); i++) {
+            Time current = input.get(i);
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (input.get(j).compareTo(current) < 0)
+                    input.set(j + 1, input.get(j));
+                else
+                    break;
+            }
+            input.set(j + 1, current);
+        }
+
+        // writing to output file
+        try {
+            FileWriter writer = new FileWriter(outputName);
+            for (Time time : input)
+                writer.write(time.toString() + "\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("File create failed");
+        }
     }
 
     /**
@@ -99,7 +207,29 @@ public class JavaTasks {
      * 121.3
      */
     static public void sortTemperatures(String inputName, String outputName) {
-        throw new NotImplementedError();
+        List<Float> input = new ArrayList<>();
+
+        // reading from input file
+        try {
+            Scanner scanner = new Scanner(new File(inputName));
+            while (scanner.hasNext())
+                input.add(Float.parseFloat(scanner.nextLine()));
+            scanner.close();
+        } catch (IOException e) {
+            System.out.println("File open failed");
+        }
+
+        Collections.sort(input);
+
+        // writing to output file
+        try {
+            FileWriter writer = new FileWriter(outputName);
+            for (float i : input)
+                writer.write(i + "\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("File create failed");
+        }
     }
 
     /**
@@ -147,9 +277,19 @@ public class JavaTasks {
      * first = [4 9 15 20 28]
      * second = [null null null null null 1 3 9 13 18 23]
      *
-     * Результат: second = [1 3 4 9 9 13 15 20 23 28]
+     * Результат: second = [1 3 4 9 9 13 15 18 20 23 28]
      */
     static <T extends Comparable<T>> void mergeArrays(T[] first, T[] second) {
-        throw new NotImplementedError();
+        int i = 0; // index for first
+        int j = first.length; // index for second
+        int z = 0; // index for "new" second
+
+        // merge sort: O(Nln(N))
+        while (z < second.length) {
+            if (j >= second.length || (i < first.length && first[i].compareTo(second[j]) < 0))
+                second[z++] = first[i++];
+            else
+                second[z++] = second[j++];
+        }
     }
 }
